c704c7b5b33f92a3bf602260bb1511ff2bbc074b
Adrian <pogorzelski.adrian@gmail.com>
2023-05-14 09:05:03 +0200

    create, formularz z lista

--- src/main/java/wsb/bugtracker/controllers/ProjectController.java
+++ src/main/java/wsb/bugtracker/controllers/ProjectController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import wsb.bugtracker.filters.ProjectFilter;
@@ -35,6 +36,24 @@ public class ProjectController {
         return modelAndView;
     }
 
+    @GetMapping("/create")
+    ModelAndView create() {
+        ModelAndView modelAndView = new ModelAndView("/projects/create");
 
+        Project newProject = new Project();
+        newProject.setEnabled(true);
+        modelAndView.addObject("project", newProject);
+
+        List<Person> people = personService.findAll();
+        modelAndView.addObject("people", people);
+
+        return modelAndView;
+    }
+
+    @PostMapping("/save")
+    ModelAndView save() {
+        ModelAndView modelAndView = new ModelAndView();
+        return modelAndView;
+    }
 
 }
